a29bea84e6c362ef8f735fef42bf7483309e37ee
Sutari Chaitra <83217099+chaitrasutari@users.noreply.github.com>
2025-04-24 21:10:26 -0400

    updated events backend

---
+++ backend/models/Event.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  capacity: {
+    type: Number,
+    default: 100
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Event', eventSchema);
